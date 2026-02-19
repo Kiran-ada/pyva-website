c849b2e2d1b353f42f5460a026def57b7a5be5ce
Kiran-ada <yuvakiranadavala@gmail.com>
2026-02-19 15:06:00 +0530

    Initial commit - pyva website

--- app.py
+++ app.py
@@ -1,28 +1,23 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
-
-app = Flask(__name__, template_folder="../templates", static_folder="../static")
-
+app = Flask(__name__)
 app.secret_key = 'pyva-2026-secret'
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def home(): return render_template('index.html')
 
 @app.route('/about')
-def about():
-    return render_template('about.html')
+def about(): return render_template('about.html')
 
 @app.route('/courses')
-def courses():
-    return render_template('courses.html')
+def courses(): return render_template('courses.html')
 
 @app.route('/placements')
-def placements():
-    return render_template('placements.html')
+def placements(): return render_template('placements.html')
+
+
 
 @app.route('/quiz')
-def quiz():
-    return render_template('quiz.html')
+def quiz(): return render_template('quiz.html')
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
@@ -30,3 +25,6 @@ def contact():
         flash('Thank you for contacting us!', 'success')
         return redirect(url_for('contact'))
     return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
